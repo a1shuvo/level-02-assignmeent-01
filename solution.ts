da679e6706d5dca88fc7c6906490f0c2cb9aca46
a1shuvo <a1shuvosaha@gmail.com>
2025-11-16 22:08:30 +0600

    Added problem 07 solution

--- solution.ts
+++ solution.ts
@@ -1,23 +1,21 @@
 // Problem: 1
-function formatValue(input: string | number | boolean) {
+type MultiType = string | number | boolean;
+function formatValue(input: MultiType): MultiType {
   if (typeof input === "string") {
     return input.toUpperCase();
   } else if (typeof input === "number") {
     return input * 10;
-  } else if (typeof input === "boolean") {
-    return !input;
   } else {
-    return;
+    return !input;
   }
 }
 
 // Problem: 2
-function getLength(input: string | (string | number)[]) {
-  if (typeof input === "string" || Array.isArray(input)) {
+function getLength(input: string | (string | number)[]): number {
+  if (typeof input === "string") {
     return input.length;
-  } else {
-    return;
   }
+  return input.length;
 }
 
 // Problem: 3
@@ -29,7 +27,7 @@ class Person {
     this.age = age;
   }
 
-  getDetails() {
+  getDetails(): string {
     return `Name: ${this.name}, Age: ${this.age}`;
   }
 }
@@ -64,7 +62,7 @@ interface Book {
   isAvailable: boolean;
 }
 
-function printBookDetails(input: Book) {
+function printBookDetails(input: Book): void {
   console.log(
     `Title: ${input.title}, Author: ${input.author}, Published: ${
       input.publishedYear
@@ -72,4 +70,39 @@ function printBookDetails(input: Book) {
   );
 }
 
+// Problem: 7
+type StringOrNumber = string | number;
+function getUniqueValues(
+  array1: StringOrNumber[],
+  array2: StringOrNumber[]
+): StringOrNumber[] {
+  const uniqueArr: StringOrNumber[] = [];
+
+  function isPresent(array: StringOrNumber[], value: StringOrNumber): boolean {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (!isPresent(uniqueArr, array1[i])) {
+      uniqueArr.push(array1[i]);
+    }
+  }
+
+  for (let i = 0; i < array2.length; i++) {
+    if (!isPresent(uniqueArr, array2[i])) {
+      uniqueArr.push(array2[i]);
+    }
+  }
+
+  return uniqueArr;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+console.log(getUniqueValues(array1, array2));
 
