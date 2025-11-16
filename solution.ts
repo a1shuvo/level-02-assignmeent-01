020d7a0756092529239167d50b3a334cba4b26e1
a1shuvo <a1shuvosaha@gmail.com>
2025-11-16 21:19:07 +0600

    Added problem 06 solution

--- solution.ts
+++ solution.ts
@@ -12,7 +12,7 @@ function formatValue(input: string | number | boolean) {
 }
 
 // Problem: 2
-function getLength(input: string | string[] | number[]) {
+function getLength(input: string | (string | number)[]) {
   if (typeof input === "string" || Array.isArray(input)) {
     return input.length;
   } else {
@@ -35,12 +35,12 @@ class Person {
 }
 
 // Problem: 4
-type Book = {
+type Item = {
   title: string;
   rating: number;
 };
 
-function filterByRating(books: Book[]): Book[] {
+function filterByRating(books: Item[]): Item[] {
   return books.filter((book) => book.rating >= 4);
 }
 
@@ -54,6 +54,22 @@ type User = {
 
 function filterActiveUsers(users: User[]): User[] {
   return users.filter((user) => user.isActive);
+}
+
+// Problem: 6
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(input: Book) {
+  console.log(
+    `Title: ${input.title}, Author: ${input.author}, Published: ${
+      input.publishedYear
+    }, Available: ${input.isAvailable ? "Yes" : "No"}`
+  );
 }
 
 
